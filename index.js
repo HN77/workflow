5e6157ea82220ba7ee989de15ca82462e18dc864
Hamid Nazari <nhamid.info@gmail.com>
2024-12-02 14:31:50 +0100

    Added Hello world route

--- index.js
+++ index.js
@@ -2,6 +2,10 @@ import express from 'express';
 
 const app = express();
 
+app.use('/', (req, res) => {
+  res.send('Hello, World!');
+});
+
 const PORT = process.env.PORT || 8888;
 app.listen(PORT, () => {
   console.log(`Server is running on port http://localhost:${PORT}`);
