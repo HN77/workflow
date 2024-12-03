01c5db42b09e93577b7caa7dadd2c5d92384723c
Hamid Nazari <nhamid.info@gmail.com>
2024-12-03 04:17:38 +0100

    added a comment

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 import app from './server.js';
 
+// Create and start the server
 const PORT = process.env.PORT || 8888;
 app.listen(PORT, () => {
   console.log(`Server is running on port http://localhost:${PORT}`);
